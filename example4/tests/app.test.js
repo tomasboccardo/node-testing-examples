dff2aa553f265570db8638e702eaad2f844b42c9
Tomás Boccardo <tomasboccardo@gmai.com>
2015-09-25 16:01:58 -0300

    Add done on example 4 as its asyncronous

--- example4/tests/app.test.js
+++ example4/tests/app.test.js
@@ -16,10 +16,11 @@ describe('File exists test', function () {
     )
   });
 
-  it('should call callback onece with parameters null and true', function () {
+  it('should call callback onece with parameters null and true', function (done) {
     var callback = sinon.spy();
     app.checkFileExists('path', callback);
     assert.isTrue(callback.calledOnce);
     assert.isTrue(callback.calledWith('No errors :D', 'This is working!'));
+    done()
   })
 });
